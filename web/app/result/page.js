a3644690a8a313b63fc30c9bea3cbfc0f76896f1
Steve-Yang-KR <73861054+Steve-Yang-KR@users.noreply.github.com>
2026-05-04 13:33:41 +0900

    Create page.js

---
+++ web/app/result/page.js
@@ -0,0 +1,12 @@
+"use client"
+
+export default function Result() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Result</h1>
+      <p>Score: 82</p>
+      <p>Balance: Good</p>
+      <p>Posture: Improve knee alignment</p>
+    </div>
+  )
+}
